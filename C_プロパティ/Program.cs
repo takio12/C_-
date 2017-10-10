0b2c161220dff2189e37f563a3ff2a152aa96a97
TAKIO-PC\takio <taka21centry@gmail.com>
2017-10-10 23:19:44 +0900

    例をかいてみた

--- C_プロパティ/Program.cs
+++ C_プロパティ/Program.cs
@@ -6,10 +6,26 @@ using System.Threading.Tasks;
 
 namespace C_プロパティ
 {
+    class Aroma
+    {
+        public double drop;
+        public double itteki
+        {
+            get { return drop; }
+            set
+            {
+                drop = value;
+            }
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
+            Aroma aroma = new Aroma();
+            aroma.itteki = 0.05;
+            Console.WriteLine("一滴あたり" + aroma.itteki + "mlです。");
         }
     }
 }
